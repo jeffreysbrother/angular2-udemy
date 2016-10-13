14525500797000fbae0853bf82d2903ea7ade659
James Cool <jeffreysbrother@gmail.com>
2016-10-13 16:00:30 -0700

    hooks and stuff I don't understand

---
+++ src/app/lifecycle.component.ts
@@ -0,0 +1,62 @@
+import { Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  template: `
+    <p>
+      lifecycle Works!
+    </p>
+  `,
+  styles: []
+})
+export class LifecycleComponent implements OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngOnChanges() {
+    this.log('ngOnChanges');
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+    console.log(hook);
+  }
+
+
+}
