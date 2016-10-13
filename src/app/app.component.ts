d4b5d859c05223862d26389ba5f3893d7e4a1305
James Cool <jeffreysbrother@gmail.com>
2016-10-13 11:27:13 -0700

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'this is green because of component styles!';
+  title = 'Root Component';
 }
